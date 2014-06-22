be0dea518bcf7b103fc054a84428dd44c57aa7f6
Riley Watkins <riley@rileywatkins.com>
2014-06-21 19:17:21 -0700

    Add comments

--- main.go
+++ main.go
@@ -14,9 +14,11 @@ func server(startCh chan bool, shutdownCh chan bool) {
         log.Fatal(err)
     }
     log.Println("(server) Server started. Listening...")
+    // Signal server has started
     startCh <- true
 
     log.Println("(server) Accepting")
+    // Wait for a connection to be made
     readConn, acceptErr := ln.Accept()
     if acceptErr != nil {
         log.Fatal(acceptErr)
@@ -24,6 +26,7 @@ func server(startCh chan bool, shutdownCh chan bool) {
     log.Println("(server) Got connection")
 
     log.Println("(server) Reading")
+    // Read one line from the connection
     line, readErr := bufio.NewReader(readConn).ReadString('\n')
     if readErr != nil {
         log.Fatal(readErr)
@@ -37,6 +40,7 @@ func server(startCh chan bool, shutdownCh chan bool) {
     }
 
     log.Println("(server) Bye!")
+    // Signal server has finished
     shutdownCh <- true
 }
 
@@ -65,9 +69,18 @@ func client() {
 func main() {
     serverStartCh := make(chan bool)
     serverShutdownCh := make(chan bool)
+
+    // Start server in a separate routine
     go server(serverStartCh, serverShutdownCh)
     log.Println("Waiting for server to start")
+
+    // Wait for server to start by blocking on the "start" channel
     <-serverStartCh
+
+    // Launch client in current routine
     client()
+
+    // Wait for server to quit before exiting by blocking on the
+    // "shutdown" channel
     <-serverShutdownCh
 }
